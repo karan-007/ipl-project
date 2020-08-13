4ea8cbcd8857e427f4c3d1d292c1ac51fcf0300c
karan chouhan <karan.chouhan@mountblue.tech>
2020-08-13 16:40:00 +0530

    refractored config.js with helper function

--- src/server/config.js
+++ src/server/config.js
@@ -12,16 +12,6 @@ const MOST_MAN_OF_MATCH_JSON_PATH = "../public/output/mostManOfMatch.json";
 const STRIKERATE_JSON_PATH = "../public/output/strikeRateOfBatsman.json";
 const BATSMAN_VS_BOWLER = "../public/output/batsmanVsBowler.json";
 
-// console.log(process.env.HOST);
-// console.log(process.env.USERNAME);
-// console.log(process.env.PASSWORD);
-// console.log(process.env.DATABASE);
-
-const hostname = process.env.HOST,
-    username = process.env.USERNAME,
-    password = process.env.PASSWORD,
-    databasename = process.env.DATABASE
-
 let con = mysql.createConnection({
     multipleStatements: true,
     host: process.env.HOST,
@@ -30,118 +20,37 @@ let con = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-// let con = mysql.createConnection({
-//     multipleStatements: true,
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database: 'ipl'
-// });
-
 con.connect();
 
-con.query("DROP TABLE IF EXISTS matches",
-    (err, drop) => {
+var matchesPlayedPerYear = "select season,count(*)as matchesPlayed from matches group by season";
+runquery(matchesPlayedPerYear, MATCHES_PLAYED_JSON_PATH);
+
+var toss = "select winner as team,count(*)as count from matches where toss_winner = winner group by winner";
+runquery(toss, TOSS_WON_MATCH_WON_JSON_PATH);
+
+var playerOfMatch = "select season,max(count)as countPOM from (select season,player_of_match,count(player_of_match+player_of_match) as count from matches group by season,player_of_match order by count(player_of_match) desc )p group by season";
+runquery(playerOfMatch, MOST_MAN_OF_MATCH_JSON_PATH);
+
+var batsmanVsBowler = `select player_dismissed as batsman,bowler,count(bowler)as count from deliveries where player_dismissed!='' and dismissal_kind!="run out" group by player_dismissed,bowler order by count(bowler) desc limit 12`;
+runquery(batsmanVsBowler, BATSMAN_VS_BOWLER);
+
+var economySuperOver = `select bowler,(sum(total_runs)-(sum(bye_runs)+sum(legbye_runs)))*6/(count(bowler)-count(case when wide_runs!=0 then 1 else null end)-count(case when noball_runs!=0 then 1 else null end)) as economy from deliveries where is_super_over=1 group by bowler`;
+runquery(economySuperOver, BEST_ECONOMY_SUPER_OVER_JSON_PATH);
+
+var strikeRate = `select season,(sum(batsman_runs)*100/(count(batsman)-count(case when wide_runs!=0 then 1 else null end)))as strikeRate from matches join deliveries on matches.id=deliveries.match_id where batsman="MS Dhoni" group by season`;
+runquery(strikeRate, STRIKERATE_JSON_PATH);
+
+function runquery(query, path) {
+    con.query(query, (err, results, fields) => {
         if (err) {
-            console.log("ERROR: ", err);
+            console.log(err);
         } else {
-            console.log("matches created");
+            saveResultToOutput(results, path);
+            console.log("excecuted");
         }
-    });
+    })
 
-con.query("DROP TABLE IF EXISTS deliveries",
-    (err, drop) => {
-        if (err) {
-            console.log("ERROR: ", err);
-        } else {
-            console.log("deliveries created");
-        }
-    });
-
-con.query("CREATE TABLE matches(id int,season int, city varchar(50),date date,team1 varchar(50),team2 varchar(50) ,toss_winner varchar(50),toss_decision varchar(50),result varchar(25),dl_applied int,winner varchar(50),win_by_runs int,win_by_wickets int,player_of_match varchar(50),venue varchar(100),umpire1 varchar(50),umpire2 varchar(50),umpire3 varchar(50))",
-    (err, drop) => {
-        if (err) {
-            console.log("ERROR: ", err);
-        } else {
-            console.log("done");
-        }
-    });
-
-con.query("CREATE TABLE deliveries(match_id int,inning int,batting_team varchar(100),bowling_team varchar(100),over int,ball int,batsman varchar(100),non_striker varchar(100),bowler varchar(100),is_super_over int,wide_runs int,bye_runs int,legbye_runs int,noball_runs int,penalty_runs int,batsman_runs int,extra_runs int,total_runs int,player_dismissed varchar(100),dismissal_kind varchar(100),fielder varchar(100))",
-    (err, drop) => {
-        if (err) {
-            console.log("ERROR: ", err);
-        } else {
-            console.log("done");
-        }
-    });
-
-con.query(`LOAD DATA LOCAL INFILE "../data/matches.csv" INTO TABLE matches FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS`,
-    (err, drop) => {
-        if (err) {
-            console.log("ERROR: ", err);
-        } else {
-            console.log("Loaded data into matches");
-        }
-    });
-
-con.query(`LOAD DATA LOCAL INFILE "../data/deliveries.csv" INTO TABLE deliveries FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS`,
-    (err, drop) => {
-        if (err) {
-            console.log("ERROR: ", err);
-        } else {
-            console.log("Loaded data into deliveries");
-        }
-    });
-
-con.query("select season,count(*)as matchesPlayed from matches group by season", (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, MATCHES_PLAYED_JSON_PATH)
-    }
-})
-
-con.query("select winner as team,count(*)as count from matches where toss_winner = winner group by winner", (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, TOSS_WON_MATCH_WON_JSON_PATH)
-    }
-})
-
-con.query("select season,max(count)as countPOM from (select season,player_of_match,count(player_of_match+player_of_match) as count from matches group by season,player_of_match order by count(player_of_match) desc )p group by season", (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, MOST_MAN_OF_MATCH_JSON_PATH)
-    }
-})
-
-con.query(`select player_dismissed as batsman,bowler,count(bowler)as count from deliveries where player_dismissed!='' and dismissal_kind!="run out" group by player_dismissed,bowler order by count(bowler) desc limit 12`, (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, BATSMAN_VS_BOWLER)
-    }
-})
-
-con.query(`select bowler,(sum(total_runs)-(sum(bye_runs)+sum(legbye_runs)))*6/(count(bowler)-count(case when wide_runs!=0 then 1 else null end)-count(case when noball_runs!=0 then 1 else null end)) as economy from deliveries where is_super_over=1 group by bowler`, (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, BEST_ECONOMY_SUPER_OVER_JSON_PATH)
-    }
-})
-
-con.query(`select season,(sum(batsman_runs)*100/(count(batsman)-count(case when wide_runs!=0 then 1 else null end)))as strikeRate from matches join deliveries on matches.id=deliveries.match_id where batsman="MS Dhoni" group by season`, (err, results, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        saveResultToOutput(results, STRIKERATE_JSON_PATH)
-    }
-})
-
+}
 function saveResultToOutput(result, path) {
     const jsonString = JSON.stringify(result);
     fs.writeFile(path, jsonString, "utf8", (err) => {
